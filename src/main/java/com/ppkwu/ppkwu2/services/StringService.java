f90db0e12fa5923a11889e47bf7e5b1614875046
kajetan.barwinski <222399@edu.p.lodz.pl>
2021-10-21 13:38:34 +0200

    Added implementation of StringService interface countCharacterOccurences method.

--- src/main/java/com/ppkwu/ppkwu2/services/StringService.java
+++ src/main/java/com/ppkwu/ppkwu2/services/StringService.java
@@ -1,4 +1,37 @@
 package com.ppkwu.ppkwu2.services;
 
-public class StringService {
+import com.ppkwu.ppkwu2.serviceinterfaces.StringServiceInterface;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StringService implements StringServiceInterface {
+
+    @Override
+    public Map<String, Integer> countCharacterOccurences(String input) {
+        HashMap<String, Integer> characterOccurencesMap = new HashMap<>();
+
+        characterOccurencesMap.put("Digits", 0);
+        characterOccurencesMap.put("Letters", 0);
+        characterOccurencesMap.put("Whitespaces", 0);
+        characterOccurencesMap.put("Special characters", 0);
+
+        for (int i = 0; i < input.length(); i++) {
+            if (Character.isDigit(input.charAt(i))) {
+                characterOccurencesMap.put("Digits",
+                        characterOccurencesMap.get("Digits") + 1);
+            } else if (Character.isLetter(input.charAt(i))) {
+                characterOccurencesMap.put("Letters",
+                        characterOccurencesMap.get("Letters") + 1);
+            } else if (Character.isWhitespace(input.charAt(i))) {
+                characterOccurencesMap.put("Whitespaces",
+                        characterOccurencesMap.get("Whitespaces") + 1);
+            } else {
+                characterOccurencesMap.put("Special characters",
+                        characterOccurencesMap.get("Special characters") + 1);
+
+            }
+        }
+        return characterOccurencesMap;
+    }
 }
