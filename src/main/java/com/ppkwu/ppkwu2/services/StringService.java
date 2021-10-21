58cf1b2f82ed479eb30d5b30c293b5820669dc3d
kajetan.barwinski <222399@edu.p.lodz.pl>
2021-10-21 13:49:25 +0200

    Implemented counting of lower and uppercase letters in StringService.

--- src/main/java/com/ppkwu/ppkwu2/services/StringService.java
+++ src/main/java/com/ppkwu/ppkwu2/services/StringService.java
@@ -14,7 +14,8 @@ public class StringService implements StringServiceInterface {
         HashMap<String, Integer> characterOccurencesMap = new HashMap<>();
 
         characterOccurencesMap.put("Digits", 0);
-        characterOccurencesMap.put("Letters", 0);
+        characterOccurencesMap.put("lowercase letters", 0);
+        characterOccurencesMap.put("uppercase letters", 0);
         characterOccurencesMap.put("Whitespaces", 0);
         characterOccurencesMap.put("Special characters", 0);
 
@@ -25,6 +26,12 @@ public class StringService implements StringServiceInterface {
             } else if (Character.isLetter(input.charAt(i))) {
                 characterOccurencesMap.put("Letters",
                         characterOccurencesMap.get("Letters") + 1);
+                if (Character.isUpperCase(input.charAt(i)))
+                    characterOccurencesMap.put("lowercase letters",
+                            characterOccurencesMap.get("lowercase letters") + 1);
+                else
+                    characterOccurencesMap.put("uppercase letters",
+                            characterOccurencesMap.get("uppercase letters") + 1);
             } else if (Character.isWhitespace(input.charAt(i))) {
                 characterOccurencesMap.put("Whitespaces",
                         characterOccurencesMap.get("Whitespaces") + 1);
