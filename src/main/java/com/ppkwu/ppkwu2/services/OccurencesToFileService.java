3ea233e643d84104ba032dfee1209ef147435748
kajetan.barwinski <222399@edu.p.lodz.pl>
2021-11-04 13:09:49 +0100

    Implemented showing character occurences map in json format.

--- src/main/java/com/ppkwu/ppkwu2/services/OccurencesToFileService.java
+++ src/main/java/com/ppkwu/ppkwu2/services/OccurencesToFileService.java
@@ -1,5 +1,8 @@
 package com.ppkwu.ppkwu2.services;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import com.ppkwu.ppkwu2.serviceinterfaces.OccurencesToFileInterface;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +16,14 @@ public class OccurencesToFileService implements OccurencesToFileInterface {
         if(format.equals("txt")){
             for(Map.Entry<String, Integer> entry : input.entrySet()){
                 result.append(entry.getKey()).append(":").append(entry.getValue()).append("\n");
+            }
+        }
+        else if(format.equals("json")){
+            ObjectMapper objectMapper = new ObjectMapper();
+            try{
+                result.append(objectMapper.writeValueAsString(input));
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
             }
         }
         return result.toString();
